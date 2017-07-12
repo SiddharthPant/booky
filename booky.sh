4bbee24bb4703cac7eeb6fd040cc186b96f3d413
Siddharth Pant <spsiddharthpant@gmail.com>
2017-07-12 11:34:21 +0530

    Customizable extract file name

--- booky.sh
+++ booky.sh
@@ -4,10 +4,11 @@
 cd $(dirname "$1")
 pdf=$(basename "$1")
 pdf_data="${pdf%.*}""_data.txt"
+EXTRACT_FILE=booky_bookmarks_extract
 bkFile="$2"
 
 echo "Converting $bkFile to pdftk compatible format"
-python3 booky.py < "$bkFile" > bookmarks.txt
+python3 booky.py < "$bkFile" > $EXTRACT_FILE
 
 echo "Dumping pdf meta data..."
 pdftk "$pdf" dump_data_utf8 output "$pdf_data"
@@ -16,10 +17,10 @@ echo "Clear dumped data of any previous bookmarks"
 sed -i '/Bookmark/d' "$pdf_data"
 
 echo "Inserting your bookmarks in the data"
-sed -i '/NumberOfPages/r bookmarks.txt' "$pdf_data"
+sed -i '/NumberOfPages/r $EXTRACT_FILE' "$pdf_data"
 
 echo "Creating new pdf with your bookmarks..."
 pdftk "$pdf" update_info_utf8 "$pdf_data" output "${pdf%.*}""_new.pdf"
 
 echo "Deleting leftovers"
-rm bookmarks.txt "$pdf_data"
+rm $EXTRACT_FILE "$pdf_data"

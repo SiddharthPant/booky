d5487230616b75b4469e397eaf8c0f3dfb10b8c7
zhoujinze <zhoujinze@gmail.com>
2022-07-25 17:45:16 +0800

    Select the sed/gsed command depending on whether it is a MACOS system or not

--- booky.sh
+++ booky.sh
@@ -8,6 +8,13 @@ pdf_data="${pdf%.*}""_data.txt"
 EXTRACT_FILE=booky_bookmarks_extract
 bkFile="$2"
 
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SED=gsed
+else
+    SED=sed
+fi
+
 echo "Converting $bkFile to pdftk compatible format"
 python3 $base/booky.py < "$bkFile" > "$EXTRACT_FILE"
 
@@ -15,10 +22,10 @@ echo "Dumping pdf meta data..."
 pdftk "$pdf" dump_data_utf8 output "$pdf_data"
 
 echo "Clear dumped data of any previous bookmarks"
-sed -i '/Bookmark/d' "$pdf_data"
+$SED -i '/Bookmark/d' "$pdf_data"
 
 echo "Inserting your bookmarks in the data"
-sed -i "/NumberOfPages/r $EXTRACT_FILE" "$pdf_data"
+$SED -i "/NumberOfPages/r $EXTRACT_FILE" "$pdf_data"
 
 echo "Creating new pdf with your bookmarks..."
 pdftk "$pdf" update_info_utf8 "$pdf_data" output "${pdf%.*}""_new.pdf"
